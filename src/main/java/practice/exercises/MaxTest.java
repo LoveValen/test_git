8001b0884ee22d39b7aba7ef3cf88700ae19a593
luofangwei <1462449970@qq.com>
2026-03-14 16:01:27 +0800

    Refine MaxTest exercise

--- src/main/java/practice/exercises/MaxTest.java
+++ src/main/java/practice/exercises/MaxTest.java
@@ -3,15 +3,8 @@ package practice.exercises;
 public class MaxTest {
     public static int max(int[] arr) {
         if (arr == null || arr.length == 0) {
-            throw new IllegalArgumentException("数组需要存在值!!!");
+            throw new IllegalArgumentException("数组不能为空!");
         }
-//        int max = arr[0];
-//        for (int i = 1; i < arr.length; i++) {
-//            if (arr[i] > max) {
-//                max = arr[i];
-//            }
-//        }
-//        return max;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
